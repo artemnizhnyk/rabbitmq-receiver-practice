f452e655656657f4f942efefaddf7d3454ce0f9e
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-07-03 18:41:37 +0200

    initial commit

---
+++ src/main/java/com/artemnizhnyk/rabbitmqreceiverpractice/RabbitmqReceiverPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.artemnizhnyk.rabbitmqreceiverpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqReceiverPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqReceiverPracticeApplication.class, args);
+    }
+
+}
